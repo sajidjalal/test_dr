d0482507512b157eedf0578d958800ff4c457efa
sajid jalal <sajid@enirmaan.com>
2025-12-06 19:59:29 +0530

    missing code added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         using: function () {
             Route::middleware('web')->prefix('doctor')->group(base_path('routes/doctor_web_route.php'));
+            Route::middleware('web')->group(base_path('routes/web.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
--- routes/doctor_web_route.php
+++ routes/doctor_web_route.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 
-
-// 
+Route::get('/test', function () {
+    return view('welcome');
+});
